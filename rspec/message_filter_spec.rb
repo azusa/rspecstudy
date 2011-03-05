874f71cbf17a2c9b67760b3fcb216750e9de24d9
大中 浩行 <azusa@fieldnotes.jp>
2011-03-05 11:37:36 +0900

    detectメソッドのテストを追加した

--- rspec/message_filter_spec.rb
+++ rspec/message_filter_spec.rb
@@ -9,5 +9,9 @@ describe MessageFilter do
 		filter.detect?('hello from foo').should ==true
 	end
 
+	it 'shold not detect message without NG word' do
+		filter = MessageFilter.new('foo')
+		filter.detect?('hello. world').should == false
+	end
 end
 
